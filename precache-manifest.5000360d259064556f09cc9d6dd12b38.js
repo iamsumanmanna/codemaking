3feb9d03c77bfd8ff708185bc33c6b7bfdf3f22e
Suman <iamsuman1000@gmail.com>
2020-09-19 23:16:34 +0530

    Updates

---
+++ precache-manifest.5000360d259064556f09cc9d6dd12b38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96b6eb85562c25299548e7ee9dcaa8f",
+    "url": "/codemaking/index.html"
+  },
+  {
+    "revision": "c5fa3ce256a77accb748",
+    "url": "/codemaking/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5171c606f8512e0ccbac",
+    "url": "/codemaking/static/css/main.bce1ee3d.chunk.css"
+  },
+  {
+    "revision": "c5fa3ce256a77accb748",
+    "url": "/codemaking/static/js/2.4f78dc36.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/codemaking/static/js/2.4f78dc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5171c606f8512e0ccbac",
+    "url": "/codemaking/static/js/main.d159f327.chunk.js"
+  },
+  {
+    "revision": "48cb0e12c4b77008372e",
+    "url": "/codemaking/static/js/runtime-main.b88880c8.js"
+  },
+  {
+    "revision": "bfdffae58030c0a09db420ac329feb72",
+    "url": "/codemaking/static/media/ReactJs1.bfdffae5.jpg"
+  },
+  {
+    "revision": "f493683d26feb76698bf30dfe2bfaa86",
+    "url": "/codemaking/static/media/php.f493683d.jpg"
+  },
+  {
+    "revision": "5cc8adaa056e75ea208a53538e065122",
+    "url": "/codemaking/static/media/phpcore.5cc8adaa.jpg"
+  },
+  {
+    "revision": "b0783c4d1922281112ef7be405824f59",
+    "url": "/codemaking/static/media/reactjs10.b0783c4d.svg"
+  },
+  {
+    "revision": "3eca8cab5c695daf77aec36bc17d5091",
+    "url": "/codemaking/static/media/wordpress.3eca8cab.jpg"
+  }
+]);
